88ce5bdabab44a299b014b10f2f03e2337534928
Alec Blance <17378596+AlecBlance@users.noreply.github.com>
2023-06-27 11:31:47 +0800

    Update for 1.8 unicafe step3

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,6 +6,23 @@ const Button = ({handleClick, text}) => <button onClick={handleClick}>{text}</bu
 
 const Counter = ({text, count}) => <p>{text} {count}</p>
 
+const Statistics = ({good, neutral, bad}) => {
+  const allCount = bad + good + neutral;
+  const averageCount = (good - bad) / allCount  || 0
+  const positivePercent = good / allCount * 100  || 0
+
+  return (
+    <>
+      <Counter text='good' count={good} />
+      <Counter text='neutral' count={neutral} />
+      <Counter text='bad' count={bad} />
+      <Counter text='all' count={allCount} />
+      <Counter text='average' count={averageCount} />
+      <Counter text='positive' count={`${positivePercent} %`} /> 
+    </>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -15,9 +32,6 @@ const App = () => {
   const clickGood = () => setGood(good + 1)
   const clickNeutral = () => setNeutral(neutral + 1)
   const clickBad = () => setBad(bad + 1)
-  const allCount = () => bad + good + neutral;
-  const averageCount = () => (good - bad) / allCount()  || 0
-  const positivePercent = () => good / allCount() * 100  || 0
 
   return (
     <div>
@@ -26,12 +40,7 @@ const App = () => {
       <Button handleClick={clickNeutral} text="neutral" />
       <Button handleClick={clickBad} text="bad" />
       <Header text='statistics' />
-      <Counter text='good' count={good} />
-      <Counter text='neutral' count={neutral} />
-      <Counter text='bad' count={bad} />
-      <Counter text='all' count={allCount()} />
-      <Counter text='average' count={averageCount()} />
-      <Counter text='positive' count={`${positivePercent()} %`} />
+      <Statistics good={good} neutral={neutral} bad={bad} /> 
     </div>
   )
 }
