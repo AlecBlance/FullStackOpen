10d839f5e8c03acf9c6946baa650e6adbaaf47c1
Alec Blance <17378596+AlecBlance@users.noreply.github.com>
2023-06-27 10:58:07 +0800

    Change App.js for 1.6 unicafe step1

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,14 +1,31 @@
 import { useState } from 'react'
 
+const Header = ({text}) => <h1>{text}</h1>
+
+const Button = ({handleClick, text}) => <button onClick={handleClick}>{text}</button> 
+
+const Counter = ({text, count}) => <p>{text} {count}</p>
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const clickGood = () => setGood(good + 1)
+  const clickNeutral = () => setNeutral(neutral + 1)
+  const clickBad = () => setBad(bad + 1)
+
   return (
     <div>
-      code here
+      <Header text='give feedback' />
+      <Button handleClick={clickGood} text="good" />
+      <Button handleClick={clickNeutral} text="neutral" />
+      <Button handleClick={clickBad} text="bad" />
+      <Header text='statistics' />
+      <Counter text='good' count={good} />
+      <Counter text='neutral' count={neutral} />
+      <Counter text='bad' count={bad} />
     </div>
   )
 }
