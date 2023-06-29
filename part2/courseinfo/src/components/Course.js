f43682279bc752fdb01bc0d27938c8032fbcad7a
Alec Blance <17378596+AlecBlance@users.noreply.github.com>
2023-06-29 17:21:18 +0800

    Update for 2.2 step7 and 2.3 step8

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -4,11 +4,14 @@ const Header = ({name}) => <h1>{name}</h1>
 
 const Content = ({parts}) => parts.map(({id, name, exercises}) => <Part key={id} name={name} exercises={exercises}/>)
 
+const Total = ({parts}) => <b><p>total of {parts.reduce((accu, {exercises}) => accu += exercises, 0)} exercises</p></b>; 
+
 const Course = ({course: {name, parts}}) => {
     return (
         <>
-            <Header name={name}/>
-            <Content parts={parts}/>
+            <Header name={name} />
+            <Content parts={parts} />
+            <Total parts={parts} />
         </>
     )
 }
