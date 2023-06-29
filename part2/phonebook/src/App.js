53f7852093b683af083427483ddc242004dc68ad
Alec Blance <17378596+AlecBlance@users.noreply.github.com>
2023-06-29 22:59:03 +0800

    Update for 2.8 and 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,36 +2,56 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
 
-  const handleInput = (e) => {
+  const handleNameInput = (e) => {
     setNewName(e.target.value)
+  }
+
+  const handleNumberInput = (e) => {
+    setNewNumber(e.target.value)
+  }
+
+  const handleFilterInput = (e) => {
+    setFilter(e.target.value)
   }
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    if (persons.some(({name}) => name == newName)) {
+    if (persons.some(({name}) => name === newName)) {
       alert(`${newName} is already added to phonebook`)
       return
     }
-    setPersons(persons.concat({name: newName}))
+    setPersons(persons.concat({name: newName, number: newNumber}))
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+          filter shown with: <input value={filter} onChange={handleFilterInput}/>
+      </div>
+      <h3>add a new</h3>
       <form onSubmit={handleSubmit}>
         <div>
-          name: <input onChange={handleInput}/>
+          name: <input value={newName} onChange={handleNameInput}/>
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberInput}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(({name}) => <p key={name}>{name}</p>)}
+      {persons.filter(({name}) => name.toLowerCase().includes(filter.toLowerCase())).map(({name, number}) => <p key={name}>{name} {number}</p>)}
     </div>
   )
 }
