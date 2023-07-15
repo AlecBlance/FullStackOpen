b18e69de4325e3b9cdf0fd82ea1686388d53e95b
Alec Blance <17378596+AlecBlance@users.noreply.github.com>
2023-07-15 16:36:39 +0800

    Update for 4.5

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,7 +2,16 @@ const dummy = (blogs) => blogs.length;
 
 const totalLikes = (blogs) => blogs.reduce((accu, { likes }) => accu + likes, 0);
 
+const favoriteBlog = (blogs) => {
+  const highestLikes = Math.max(...blogs.map(({ likes }) => likes));
+  const { title, author, likes } = blogs.find(
+    (blog) => blog.likes === highestLikes,
+  );
+  return { title, author, likes };
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
