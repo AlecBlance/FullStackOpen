9fb9911c264469a97dafdb104c82632c1cc19bfd
Alec Blance <17378596+AlecBlance@users.noreply.github.com>
2023-07-15 17:25:44 +0800

    Update for 4.6

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash');
+
 const dummy = (blogs) => blogs.length;
 
 const totalLikes = (blogs) => blogs.reduce((accu, { likes }) => accu + likes, 0);
@@ -10,8 +12,17 @@ const favoriteBlog = (blogs) => {
   return { title, author, likes };
 };
 
+const mostBlogs = (blogs) => {
+  const blogsByAuthor = _.groupBy(blogs, 'author');
+  const blogCount = _.map(blogsByAuthor,
+    (blog, author) => ({ author, blogs: blog.length })
+  );
+  return _.maxBy(blogCount, 'blogs');
+};
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs,
 };
