190099f4723785fa7a8e0e420ab908e91aedf209
Alec Blance <17378596+AlecBlance@users.noreply.github.com>
2023-07-22 13:06:56 +0800

    Update for 4.17

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,11 +1,13 @@
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable consistent-return */
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
+const User = require('../models/user');
 
 require('express-async-errors');
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 });
   response.json(blogs);
 });
 
@@ -13,8 +15,13 @@ blogsRouter.post('/', async (request, response) => {
   const { body } = request;
   if (!body.likes) body.likes = 0;
   if (!body.title || !body.url) return response.status(400).end();
-  const blog = new Blog(body);
+  const { _id } = (await User.aggregate([{ $sample: { size: 1 } }]))[0];
+  body.user = _id;
+  const blog = await new Blog(body).populate('user', { username: 1, name: 1 });
   const result = await blog.save();
+  const user = await User.findById(_id);
+  user.blogs = user.blogs.concat(result._id);
+  user.save();
   response.status(201).json(result);
 });
 
--- part4/controllers/users.js
+++ part4/controllers/users.js
@@ -24,7 +24,7 @@ usersRouter.post('/', async (req, res, next) => {
 });
 
 usersRouter.get('/', async (req, res) => {
-  const users = await User.find({});
+  const users = await User.find({}).populate('blogs', { user: 0, likes: 0 });
   res.json(users);
 });
 
