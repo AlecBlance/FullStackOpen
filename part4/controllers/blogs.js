ef6b4a8210ec34f9de25deb5c21913fd47351748
Alec Blance <17378596+AlecBlance@users.noreply.github.com>
2023-07-22 15:37:02 +0800

    Refactor

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -15,20 +15,30 @@ blogsRouter.post('/', async (request, response) => {
   if (!body.likes) body.likes = 0;
   if (!body.title || !body.url) return response.status(400).end();
   if (!user) return response.status(401).json({ error: 'token invalid' });
-  body.user = user._id;
-  const blog = await new Blog(body).populate('user', { username: 1, name: 1 });
+
+  const newBlog = {
+    ...body,
+    user: user._id,
+  };
+
+  const blog = await new Blog(newBlog).populate('user', { username: 1, name: 1 });
   const result = await blog.save();
+
   user.blogs = user.blogs.concat(result._id);
   user.save();
+
   response.status(201).json(result);
 });
 
 blogsRouter.delete('/:id', async (request, response) => {
   const { user, params: { id } } = request;
   if (!user) return response.status(401).json({ error: 'token invalid' });
+
   const blogCreator = (await Blog.findById(id)).user;
   if (blogCreator.toString() !== user._id.toString()) return response.status(400).json({ error: 'Missing permission' });
+
   await Blog.findByIdAndRemove(id);
+
   response.status(204).end();
 });
 
