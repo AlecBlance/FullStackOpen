97e110a63ac4496d47e8c98f069354a84ea30c9b
Alec Blance <17378596+AlecBlance@users.noreply.github.com>
2023-07-25 22:28:38 +0800

    Update for 5.1

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,9 +1,16 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [user, setUser] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,12 +18,112 @@ const App = () => {
     )  
   }, [])
 
-  return (
+  useEffect(() => {
+    const userLogged = window.localStorage.getItem('userLogged')
+    if (userLogged) {
+      const user = JSON.parse(userLogged)
+      setUser(user)
+    }
+  }, [])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({username, password})
+      
+      window.localStorage.setItem(
+        'userLogged', JSON.stringify(user)
+      )
+      setUser(user)
+      setPassword('')
+      setUsername('')
+    } catch (exception) {
+
+    }
+  }
+
+  const loginForm = () => (
+    <div>
+      <h1>log in to application</h1>
+      <form onSubmit={handleLogin}>
+        <div>
+          username 
+          <input 
+            type="text"
+            name="Username"
+            value={username}
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password 
+          <input 
+            type="password"
+            name="Password"
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </div>
+  )
+
+  const handleBlog = async (event) => {
+    event.preventDefault()
+    // add more here 
+  }
+
+  const handleLogout = () => {
+    setUser(null)
+    window.localStorage.removeItem('userLogged')
+  }
+  
+  const blogsList = () => (
     <div>
       <h2>blogs</h2>
+      <p>{user.name} logged in <button onClick={handleLogout}>logout</button></p>
+      <h1>create new</h1>
+      <form onSubmit={handleBlog}>
+        <div>
+          title:
+          <input
+            type="text"
+            name="title"
+            value={title}
+            onChange={({ target }) => setTitle(target.value)}
+          />
+        </div>
+        <div>
+          author:
+          <input
+            type="text"
+            name="author"
+            value={author}
+            onChange={({ target }) => setAuthor(target.value)}
+          />
+        </div>
+        <div>
+          url:
+          <input
+            type="text"
+            name="url"
+            value={url}
+            onChange={({ target }) => setUrl(target.value)}
+          />
+        </div>
+        <button type="submit">create</button>
+      </form>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
+    </div>
+  )
+
+  return (
+    <div>
+      {user && blogsList()}
+      {!user && loginForm()}
     </div>
   )
 }
