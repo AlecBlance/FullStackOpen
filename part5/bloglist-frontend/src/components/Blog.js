3d85dc964bbc9f0ee355d211f4580a909c7976da
Alec Blance <17378596+AlecBlance@users.noreply.github.com>
2023-07-27 11:48:16 +0800

    feat: 5.7 - added view/hide button for each blog

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,26 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const showWhenVisible = {display: visible ? '': 'none'}
+  const viewButtonLabel = visible ? 'hide': 'view'
+
+  const toggleVisibility = () => setVisible(!visible)
+
+  return (
+    <div className='blog'>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={toggleVisibility}>{viewButtonLabel}</button>
+      </div>
+      <div style={showWhenVisible}>
+        <p>{blog.url}</p>
+        <p>likes:{blog.likes} <button>like</button></p>
+        <p>{blog.user.name}</p>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
