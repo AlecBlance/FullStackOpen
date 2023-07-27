3a7ccae2f27502438ee1367c81092bbc5f5e62fe
Alec Blance <17378596+AlecBlance@users.noreply.github.com>
2023-07-27 14:17:04 +0800

    feat: 5.9 - Added functional likes button

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,12 +1,18 @@
 import { useState } from 'react'
+import blogService from '../services/blogs'
 
 const Blog = ({ blog }) => {
   const [visible, setVisible] = useState(false)
+  const [likes, setLikes] = useState(blog.likes)
 
   const showWhenVisible = {display: visible ? '': 'none'}
   const viewButtonLabel = visible ? 'hide': 'view'
 
   const toggleVisibility = () => setVisible(!visible)
+  const handleLikes = async () => {
+    const {likes} = await blogService.like(blog.id)
+    setLikes(likes)
+  }
 
   return (
     <div className='blog'>
@@ -16,7 +22,7 @@ const Blog = ({ blog }) => {
       </div>
       <div style={showWhenVisible}>
         <p>{blog.url}</p>
-        <p>likes:{blog.likes} <button>like</button></p>
+        <p>likes:{likes} <button onClick={handleLikes}>like</button></p>
         <p>{blog.user.name}</p>
       </div>
     </div>
--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -20,9 +20,18 @@ const create = async (data) => {
   return response.data
 }
 
+const like = async (id) => {
+  const config = {
+    headers: { Authorization: token }
+  }
+  const response = await axios.put(`${baseUrl}/${id}`, null, config)
+  return response.data
+} 
+
 // eslint-disable-next-line import/no-anonymous-default-export
 export default { 
   getAll,
   create,   
   setToken,
+  like,
 }
