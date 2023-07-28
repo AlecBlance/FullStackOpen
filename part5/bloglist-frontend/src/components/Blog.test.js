1036a3adaf04228e3e4faefdd8a0d9191963335c
Alec Blance <17378596+AlecBlance@users.noreply.github.com>
2023-07-28 16:19:00 +0800

    test: 5.14 - added userEvent testing library - added test for togglable info

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 describe('<Blog />', () => {
@@ -33,5 +34,14 @@ describe('<Blog />', () => {
   test('has optional URL or number of likes by default', () => {
     const div = container.querySelector('.togglableInfo')
     expect(div).toHaveStyle('display: none')
+  })
+
+  test('url and likes shown when show button is clicked', async () => {
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
+
+    const div = container.querySelector('.togglableInfo')
+    expect(div).not.toHaveStyle('display: none')
   })
 })
