3fb7c06bfdf9bc01d6187cf9aa5c4584acb90f00
Alec Blance <17378596+AlecBlance@users.noreply.github.com>
2023-07-31 11:58:21 +0800

    test: 5.18 - added new user - added successful login - added login fail

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,6 +1,12 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'Alec Blance',
+      username: 'AlecBlance',
+      password: 'alec',
+    }
+    cy.request('POST', 'http://localhost:3003/api/users', user)
     cy.visit('http://localhost:3000')
   })
 
@@ -9,5 +15,25 @@ describe('Blog app', function() {
     cy.contains('username')
     cy.contains('password')
     cy.contains('login')
+  })
+
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('[name="Username"]').type('AlecBlance')
+      cy.get('[name="Password"]').type('alec')
+      cy.contains('login').click()
+      cy.contains('Alec Blance logged in')
+      cy.get('.error').should('not.exist')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('[name="Username"]').type('AlecBlance')
+      cy.get('[name="Password"]').type('wrong')
+      cy.contains('login').click()
+      cy.get('.error')
+        .should('contain' ,'invalid username or password')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+      cy.contains('Alec Blance logged in').should('not.exist')
+    })
   })
 })
