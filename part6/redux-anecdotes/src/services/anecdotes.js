54e0d931cf23bbd3bf5886a2b64d4b08fd4f09cd
Alec Blance <17378596+AlecBlance@users.noreply.github.com>
2023-08-04 15:01:26 +0800

    refactor: inconsistent spacing

--- part6/redux-anecdotes/src/services/anecdotes.js
+++ part6/redux-anecdotes/src/services/anecdotes.js
@@ -3,8 +3,8 @@ import axios from 'axios'
 const baseUrl = 'http://localhost:3001/anecdotes'
 
 const getAll = async () => {
-    const response = await axios.get(baseUrl)
-    return response.data
+  const response = await axios.get(baseUrl)
+  return response.data
 }
 
 // eslint-disable-next-line import/no-anonymous-default-export
