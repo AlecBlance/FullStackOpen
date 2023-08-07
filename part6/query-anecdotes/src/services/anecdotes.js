65a3afbaa958134a5734d8c86784cf87351b29e1
Alec Blance <17378596+AlecBlance@users.noreply.github.com>
2023-08-07 12:15:59 +0800

    feat: 6.21 implement working add anecdote - added mutation in AnecdoteForm component - added create function in anecdote service for adding anecdote in db

--- part6/query-anecdotes/src/services/anecdotes.js
+++ part6/query-anecdotes/src/services/anecdotes.js
@@ -2,7 +2,24 @@ import axios from 'axios'
 
 const baseUrl = 'http://localhost:3001/anecdotes'
 
+const getId = () => (100000 * Math.random()).toFixed(0)
+
+const asObject = (anecdote) => {
+  return {
+    content: anecdote,
+    id: getId(),
+    votes: 0
+  }
+}
+
 export const getAll = async () => {
     const response = await axios.get(baseUrl)
+    return response.data
+}
+
+export const create = async (content) => {
+    if (content.length < 5) return 
+    const newContent = asObject(content)
+    const response = await axios.post(baseUrl, newContent)
     return response.data
 }
