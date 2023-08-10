ff5e8fe770666edd29987affda2bae534e0af496
Alec Blance <17378596+AlecBlance@users.noreply.github.com>
2023-08-10 16:38:20 +0800

    refactor: Add other reducers

---
+++ part7/extended-bloglist/frontend/src/reducers/blogReducer.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = [];
+
+const blogSlice = createSlice({
+  name: "blog",
+  initialState,
+  reducers: {
+    setBlog(state, action) {
+      return action.payload;
+    },
+  },
+});
+
+export const { setBlog } = blogSlice.actions;
+
+export default blogSlice.reducer;
