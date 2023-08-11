9eede38fada398461666130633367f232ee5cee9
Alec Blance <17378596+AlecBlance@users.noreply.github.com>
2023-08-11 09:43:30 +0800

    fix: persistent last page visit

--- part7/extended-bloglist/frontend/src/components/UserHeaderInfo.js
+++ part7/extended-bloglist/frontend/src/components/UserHeaderInfo.js
@@ -1,12 +1,15 @@
 import { useDispatch, useSelector } from "react-redux";
 import { logout } from "../reducers/userReducer";
+import { useNavigate } from "react-router-dom";
 
 const UserHeaderInfo = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const user = useSelector((state) => state.user);
 
   const handleLogout = () => {
     dispatch(logout());
+    navigate("/");
   };
 
   return (
