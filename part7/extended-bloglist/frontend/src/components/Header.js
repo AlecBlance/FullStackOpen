961aa6d82f25c4ef7800129fcbe8d9e0b3ec4067
Alec Blance <17378596+AlecBlance@users.noreply.github.com>
2023-08-14 15:15:03 +0800

    style: 7.20 header revamp

--- part7/extended-bloglist/frontend/src/components/Header.js
+++ part7/extended-bloglist/frontend/src/components/Header.js
@@ -3,9 +3,19 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className="header">
-      <Link to={"/"}>blogs</Link>
-      <Link to={"/users"}>users</Link>
+    <div className="flex justify-between py-5 px-20">
+      <ul className="font-semibold list-none flex text-sm text-slate-200 items-center">
+        <li>
+          <Link to={"/"} className="mr-10 hover:text-teal-400">
+            Blogs
+          </Link>
+        </li>
+        <li>
+          <Link to={"/users"} className="hover:text-teal-400">
+            Users
+          </Link>
+        </li>
+      </ul>
       <UserHeaderInfo />
     </div>
   );
--- part7/extended-bloglist/frontend/src/components/UserHeaderInfo.js
+++ part7/extended-bloglist/frontend/src/components/UserHeaderInfo.js
@@ -1,11 +1,13 @@
 import { useDispatch, useSelector } from "react-redux";
 import { logout } from "../reducers/userReducer";
 import { useNavigate } from "react-router-dom";
+import Avatar, { genConfig } from "react-nice-avatar";
 
 const UserHeaderInfo = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const user = useSelector((state) => state.user);
+  const config = genConfig(user.name);
 
   const handleLogout = () => {
     dispatch(logout());
@@ -13,9 +15,21 @@ const UserHeaderInfo = () => {
   };
 
   return (
-    <p className="userHeader">
-      {user.name} logged in <button onClick={handleLogout}>logout</button>
-    </p>
+    <div className="text-sm text-slate-400 flex items-center">
+      <Avatar className="h-7 w-7" {...config} />
+      <p className="ml-2 mr-5">{user.name}</p>
+      <button onClick={handleLogout} className="h-7 w-7">
+        <svg
+          className="fill-teal-400 h-4 w-4"
+          id="Layer_1"
+          viewBox="0 0 512 512"
+          xmlns="http://www.w3.org/2000/svg"
+          data-name="Layer 1"
+        >
+          <path d="m363.335 488a24 24 0 0 1 -24 24h-226.253a80.09 80.09 0 0 1 -80-80v-352a80.09 80.09 0 0 1 80-80h226.253a24 24 0 0 1 0 48h-226.253a32.035 32.035 0 0 0 -32 32v352a32.034 32.034 0 0 0 32 32h226.253a24 24 0 0 1 24 24zm108.553-248.97-114.051-114.052a24 24 0 1 0 -33.937 33.941l73.077 73.081h-188.936a24 24 0 1 0 0 48h188.935l-73.08 73.08a24 24 0 1 0 33.941 33.941l114.051-114.05a24 24 0 0 0 0-33.941z" />
+        </svg>
+      </button>
+    </div>
   );
 };
 
