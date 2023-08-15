8415793fabf11dd6ad3e92b60ebc2aafb8e6eb29
Alec Blance <17378596+AlecBlance@users.noreply.github.com>
2023-08-16 00:45:54 +0800

    style: blog info

---
+++ part7/extended-bloglist/frontend/src/components/Post.js
@@ -0,0 +1,33 @@
+import { isValidElement } from "react";
+import Avatar, { genConfig } from "react-nice-avatar";
+
+const Post = ({ owner, content, link }) => {
+  const config = genConfig(owner);
+  return (
+    <div className="flex flex-col">
+      <div className="flex items-center mb-5">
+        <Avatar {...config} className="h-14 w-14 mr-5" />
+        <p className="font-semibold text-slate-200">{owner}</p>
+      </div>
+      {isValidElement(content) ? (
+        <div className="flex items-center m-5 border border-slate-500 p-3 rounded-lg">
+          {content}
+        </div>
+      ) : (
+        <div>
+          <h1 className="text-slate-200">{content}</h1>
+          {link && (
+            <span className="flex">
+              <p className="text-slate-200 mr-2">@</p>
+              <a className="text-teal-400 hover:text-teal-300" href={link}>
+                {link}
+              </a>
+            </span>
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Post;
