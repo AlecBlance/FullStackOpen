c21282d577dd960f66a177f77d5c2c58d3b8f3cd
Alec Blance <17378596+AlecBlance@users.noreply.github.com>
2023-08-17 15:03:47 +0800

    feat: 8.3 All authors query with bookCount field

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -98,10 +98,18 @@ const typeDefs = `
     genres: [String!]!
   }
 
+  type Authors {
+    name: String!
+    id: ID!
+    born: Int
+    bookCount: Int
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
     allBooks: [Books!]!
+    allAuthors: [Authors!]!
   }
 `;
 
@@ -110,6 +118,11 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: () => books,
+    allAuthors: () => authors,
+  },
+  Authors: {
+    bookCount: (root) =>
+      books.filter(({ author }) => author === root.name).length,
   },
 };
 
