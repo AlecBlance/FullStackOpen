008d6823f3dbf5b789b1548ff78def41fac7a0b4
Alec Blance <17378596+AlecBlance@users.noreply.github.com>
2023-08-24 10:49:02 +0800

    refactor: unused package

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -1,5 +1,4 @@
 const { ApolloServer } = require("@apollo/server");
-const { startStandaloneServer } = require("@apollo/server/standalone");
 const typeDefs = require("./graphql/typeDefs");
 const resolvers = require("./graphql/resolvers");
 const config = require("./utils/config");
