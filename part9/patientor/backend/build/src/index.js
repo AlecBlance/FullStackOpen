7731609d78fd234f072d7c688ca2472072470691
Alec Blance <17378596+AlecBlance@users.noreply.github.com>
2023-08-26 14:21:54 +0800

    feat: 9.10 add diagnoses router

---
+++ part9/patientor/backend/build/src/index.js
@@ -0,0 +1,17 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const diagnoses_1 = __importDefault(require("./routes/diagnoses"));
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
+app.use("/api/diagnoses", diagnoses_1.default);
+app.get("/api/ping", (_req, res) => {
+    res.send("pong");
+});
+app.listen(3001, () => {
+    console.log("Server running at http://localhost:3001");
+});
